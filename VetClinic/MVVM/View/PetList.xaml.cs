83e87213b5ce0e166803d9dd0935130b7a3b95a3
Ragnarson123 <91853286+Ragnarson123@users.noreply.github.com>
2025-06-14 03:32:18 +0200

    Petlist v0.1

--- VetClinic/MVVM/View/PetList.xaml.cs
+++ VetClinic/MVVM/View/PetList.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using VetClinic.MVVM.ViewModel;
 
 namespace VetClinic.MVVM.View
 {
@@ -23,6 +24,35 @@ namespace VetClinic.MVVM.View
         public PetList()
         {
             InitializeComponent();
+            this.KeyDown += PetList_KeyDown;
+            this.Focusable = true;
+        }
+
+        private void PetList_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                var viewModel = DataContext as PetListViewModel;
+                if (viewModel != null)
+                {
+                    if (viewModel.IsEditingPet)
+                    {
+                        viewModel.CancelEditCommand.Execute(null);
+                        e.Handled = true;
+                    }
+                    else if (viewModel.IsConfirmingDelete)
+                    {
+                        viewModel.CancelDeleteCommand.Execute(null);
+                        e.Handled = true;
+                    }
+                }
+            }
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Ustaw focus na kontrolce, aby obsługa klawiatury działała
+            this.Focus();
         }
     }
 }
